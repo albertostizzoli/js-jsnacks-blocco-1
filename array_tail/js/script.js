e134b2d2d8cf5389935ef8b02661a2f7b2329555
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-13 16:48:13 +0200

    Added JS

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,6 +1,44 @@
+//Array
+const numbers = [
+    '10',
+    '12',
+    '26',
+    '33',
+    '38',
+    '42',
+    '45',
+    '52',
+    '60',
+    '65',
+    '70',
+    '72',
+    '77',
+    '80',
+    '88',
+    '92',
+    '96',
+    '100',
+];
 
 
+//Ciclo
+const button = document.querySelector('button');
 
+// Creo una funzione che mi permetta di scrivere un risultato randomico
+button.addEventListener('click', function (){
+    for (let i = 0; i < numbers.length; i++) {
+        let numero;
+        let risultato;
+        numero = numbers[randomInteger(0, 5)];
+        risultato = numero;
+        console.log(risultato);
+
+        const text = document.getElementById('text-2');
+        text.innerHTML = `${numero}`;
+
+   }
+}
+);
 
 
 function randomInteger(min, max) {
